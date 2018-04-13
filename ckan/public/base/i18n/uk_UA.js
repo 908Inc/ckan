e81f28f6995c54d83f4b30b48f41d1f22c91c453
DaleDMT <daledmt@gmail.com>
2018-04-13 19:18:42 +0300

    fixed Cancel translation

---
+++ ckan/public/base/i18n/uk_UA.js
@@ -0,0 +1,179 @@
+{
+  "": {
+    "domain": "ckan",
+    "lang": "uk_UA",
+    "plural-forms": "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2)"
+  },
+  "Add Filter": [
+    null,
+    "Додати фільтр"
+  ],
+  "An Error Occurred": [
+    null,
+    "Виникла помилка"
+  ],
+  "Are you sure you want to perform this action?": [
+    null,
+    "Ви впевнені, що хочете виконати цю операцію?"
+  ],
+  "Cancel": [
+    null,
+    "Скасувати"
+  ],
+  "Confirm": [
+    null,
+    "Підтвердити"
+  ],
+  "Could not load view": [
+    null,
+    "Не вдалось завантажити представлення"
+  ],
+  "DataProxy returned an error": [
+    null,
+    "DataProxy повернув помилку"
+  ],
+  "DataStore returned an error": [
+    null,
+    "DataStore повернув помилку"
+  ],
+  "Edit": [
+    null,
+    "Редагувати"
+  ],
+  "Failed to load data API information": [
+    null,
+    "Не вдалося завантажити інформацію про дані API"
+  ],
+  "File": [
+    null,
+    "Файл"
+  ],
+  "Filters": [
+    null,
+    "Фільтри"
+  ],
+  "Follow": [
+    null,
+    "Стежити"
+  ],
+  "Graph": [
+    null,
+    "Графік"
+  ],
+  "Grid": [
+    null,
+    "Сітка"
+  ],
+  "Hide": [
+    null,
+    "Скрити"
+  ],
+  "Image": [
+    null,
+    "Зображення"
+  ],
+  "Input is too short, must be at least one character": [
+    null,
+    "Ввід занадто короткий, має бути щонайменьше один знак"
+  ],
+  "Link": [
+    null,
+    "Посилання"
+  ],
+  "Link to a URL on the internet (you can also link to an API)": [
+    null,
+    "Додати посилання на адресу в інтернеті (або посилання на API)"
+  ],
+  "Loading...": [
+    null,
+    "Завантаження..."
+  ],
+  "Map": [
+    null,
+    "Карта"
+  ],
+  "No matches found": [
+    null,
+    "Співпадінь немає"
+  ],
+  "Please Confirm Action": [
+    null,
+    "Підтвердіть операцію"
+  ],
+  "Remove": [
+    null,
+    "Прибрати"
+  ],
+  "Resource uploaded": [
+    null,
+    "Ресурс вивантажено"
+  ],
+  "Save order": [
+    null,
+    "Зберегти порядок"
+  ],
+  "Saving...": [
+    null,
+    "Збереження..."
+  ],
+  "Select a field": [
+    null,
+    "Оберіть поле"
+  ],
+  "Show more": [
+    null,
+    "Показати більше"
+  ],
+  "Start typing…": [
+    null,
+    "Друкуйте..."
+  ],
+  "There are unsaved modifications to this form": [
+    null,
+    "Ця форма містить незбережені зміни"
+  ],
+  "There is no API data to load for this resource": [
+    null,
+    "Для цього ресурсу немає доступних для завантаження даних API"
+  ],
+  "URL": [
+    null,
+    "URL"
+  ],
+  "Unable to authenticate upload": [
+    null,
+    "Не вдалося завантажити"
+  ],
+  "Unable to get data for uploaded file": [
+    null,
+    "Не вдалось отримати дані з вивантаженого файла"
+  ],
+  "Unable to upload file": [
+    null,
+    "Не вдалось вивантажити файл"
+  ],
+  "Unfollow": [
+    null,
+    "Не стежити"
+  ],
+  "Upload": [
+    null,
+    "Завантажити"
+  ],
+  "Upload a file": [
+    null,
+    "Вкласти файл"
+  ],
+  "Upload a file on your computer": [
+    null,
+    "Завантажити"
+  ],
+  "You are uploading a file. Are you sure you want to navigate away and stop this upload?": [
+    null,
+    "Ви вкладаєте файл. Ви впевнені, що хочете перейти на іншу сторінку і припинити вкладення?"
+  ],
+  "error loading view": [
+    null,
+    "помилка при завантаженні представлення"
+  ]
+}
